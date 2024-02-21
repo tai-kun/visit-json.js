90705c4137be257595ecaad45086c7a41ae92fdf
tai-kun <taikun.dev@gmail.com>
2024-02-22 08:38:03 +0900

    chore: Add plugin to automatically resolve module extensions

--- config/code/esbuild.config.js
+++ config/code/esbuild.config.js
@@ -1,13 +1,15 @@
 // @ts-check
 
 import { buildDefine } from "cfg-test/define"
+import fs from "node:fs"
+import path from "node:path"
 
 /** @type {import("esbuild").BuildOptions[]} */
 export default [
   {
     // General
 
-    format: "esm",
+    bundle: true,
     platform: "node",
 
     // Input
@@ -20,6 +22,7 @@ export default [
 
     // Output contents
 
+    format: "esm",
     lineLimit: 80,
 
     // Output location
@@ -36,5 +39,81 @@ export default [
       "Symbol",
       "Symbol.for",
     ],
+
+    plugins: [autoInsertExtenstion()],
   },
 ]
+
+/** @param {string} path */
+function isLocalFile(path) {
+  return path.startsWith(".")
+}
+
+/** @type {Record<string, boolean>} */
+const cache = {}
+
+/** @param {string} file */
+function readOk(file) {
+  if (file in cache) {
+    return cache[file]
+  }
+
+  try {
+    fs.accessSync(file, fs.constants.R_OK)
+
+    return cache[file] = true
+  } catch {
+    return cache[file] = false
+  }
+}
+
+/**
+ * @param {string} dir
+ * @param {string} file
+ */
+function getBuiltPath(dir, file) {
+  dir = path.join(dir, file)
+
+  for (const [src, dst] of Object.entries({ ".ts": ".js", ".tsx": ".jsx" })) {
+    if (readOk(dir + src)) {
+      return file + dst
+    }
+  }
+
+  return null
+}
+
+/** @returns {import("esbuild").Plugin} */
+function autoInsertExtenstion() {
+  return {
+    name: "auto-insert-extension",
+    setup(build) {
+      build.onResolve({ filter: /.*/ }, args => {
+        if (
+          args.namespace !== "file"
+          || args.kind !== "import-statement"
+          || !isLocalFile(args.path)
+        ) {
+          return null
+        }
+
+        const builtPath = getBuiltPath(args.resolveDir, args.path)
+
+        if (!builtPath) {
+          return {
+            errors: [
+              {
+                text: `File not found: ${args.path}`,
+              },
+            ],
+          }
+        }
+
+        return {
+          path: builtPath,
+          external: true,
+        }
+      })
+    },
+  }
+}
